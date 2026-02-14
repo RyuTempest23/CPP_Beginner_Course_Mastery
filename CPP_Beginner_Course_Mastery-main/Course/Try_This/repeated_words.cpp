2a9adb57a4c68579c8fccefb21deeb0ec6f90f40
RyuTempest23 <johnvencies@gmail.com>
2026-02-15 06:30:26 +0800

    Later again

---
+++ CPP_Beginner_Course_Mastery-main/Course/Try_This/repeated_words.cpp
@@ -0,0 +1,22 @@
+#include "std_library.h"
+
+int main() {
+    string current_word;
+    string previous_word = "";
+
+    std::cout << "Please enter any word or enter q to quit.\n";
+    while (std::cin >> current_word) {
+        if (current_word == "q") {
+            break;
+        }
+
+        if (previous_word == current_word) {
+            std::cout << "Repeated word: " << current_word << "\n";
+        }
+        
+        previous_word = current_word;
+    }
+
+    keep_window_open();
+    return 0;
+}
