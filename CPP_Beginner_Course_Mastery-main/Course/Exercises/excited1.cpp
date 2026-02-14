5209371a280f0879de5d71a68705ecc5502a65cd
RyuTempest23 <johnvencies@gmail.com>
2026-02-14 20:52:06 +0800

    So much changes damn

---
+++ CPP_Beginner_Course_Mastery-main/Course/Exercises/excited1.cpp
@@ -0,0 +1,7 @@
+#include "std_library.h"
+
+int main() {
+    std::cout << "Hello, programming!\n" << "Here we go!\n";
+    keep_window_open();
+    return 0;
+}
---
+++ CPP_Beginner_Course_Mastery-main/Course/Exercises/exercise2.cpp
@@ -0,0 +1,18 @@
+#include "std_library.h"
+
+int main() {
+    // TODO: Make a list of instructions for a computer to find your school's computer laboratory.
+
+    std::cout << "=== Jose Rizal Memorial State University ===\n\n";
+    std::cout << "MISSION: Find the Computer Laboratory\n\n";
+    std::cout << "1. Show your student ID to pass through the entrance.\n";
+    std::cout << "2. Four gates stand before you... Choose Gate 1 for the fastest route.\n";
+    std::cout << "3. Walk forward until you spot the CTED Student Lounge.\n";
+    std::cout << "4. Turn right and move straight ahead — the CCS Faculty Room is nearby.\n";
+    std::cout << "5. Turn left and scan the rooms beside you.\n";
+    std::cout << "6. You have discovered three labs: Lab 1, Lab 2, and Lab 3.\n";
+    std::cout << "7. Choose wisely... Which laboratory will you enter?\n";
+
+    keep_window_open();
+    return 0;
+}
